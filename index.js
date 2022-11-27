8b4c6b129469115d96cd029a7ee4ad989d9cfd5f
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-11-27 18:09:35 +0000

    server

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const path = require('path');
 const app = express();
 const port = process.env['PORT'] || 42069;
 
+// server
+app.use("/", express.static(path.join(__dirname, 'public')));
+app.listen(port);
